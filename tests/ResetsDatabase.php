81de12c6c803261e2fb3f3a2796a2d263e311da7
Artur Hanusek <arthur@youritsolutions.ie>
2024-07-25 13:30:32 +0200

    autocreate test user in dusk database

--- tests/ResetsDatabase.php
+++ tests/ResetsDatabase.php
@@ -3,6 +3,7 @@
 namespace Tests;
 
 use App;
+use App\User;
 use Illuminate\Support\Facades\Artisan;
 
 trait ResetsDatabase
@@ -19,5 +20,15 @@ trait ResetsDatabase
         App\Console\Commands\ClearDatabaseCommand::resetDatabase();
 
         Artisan::call('app:install');
+
+        User::where(['email' => 'demo-admin@ship.town', 'password' => 'secret1144'])->first();
+
+        if (empty($user)) {
+            User::factory()->create([
+                'email' => 'demo-admin@ship.town',
+                'password' => bcrypt('secret1144'),
+            ]);
+        }
+
     }
 }
