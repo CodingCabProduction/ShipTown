d4cadd5cb5e1afbb1465ed12401d986d635b16cd
Artur Hanusek <arthur@youritsolutions.ie>
2024-09-29 19:11:41 +0200

    Improve transaction page test (#589)

--- tests/Browser/Routes/Tools/DataCollector/TransactionPageTest.php
+++ tests/Browser/Routes/Tools/DataCollector/TransactionPageTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Browser\Routes\Tools\DataCollector;
 
+use App\Models\Product;
+use App\Models\ProductPrice;
 use App\Models\Warehouse;
 use App\User;
 use Laravel\Dusk\Browser;
@@ -17,29 +19,65 @@ class TransactionPageTest extends DuskTestCase
      */
     public function testPage(): void
     {
+        /** @var Warehouse $warehouse */
+        $warehouse = Warehouse::factory()->create();
+
+        Product::factory()->create(['sku' => '4001']);
+
+        ProductPrice::query()->update(['price' => 10]);
+
         /** @var User $user */
         $user = User::factory()->create([
-            'warehouse_id' => Warehouse::factory()->create()->getKey(),
+            'warehouse_code' => $warehouse->code,
+            'warehouse_id' => $warehouse->getKey(),
         ]);
+
         $user->assignRole('admin');
 
         $this->browse(function (Browser $browser) use ($user) {
             $browser->disableFitOnFailure();
-            $browser->loginAs($user);
-            $browser->visit($this->uri);
-            $browser->assertPathIs($this->uri);
-            // $browser->assertSee('');
-            $browser->assertSourceMissing('Server Error');
+            $browser->loginAs($user)
+                ->visit($this->uri)
+                ->assertPathIs($this->uri)
+                ->pause($this->shortDelay)
+                ->pause($this->shortDelay)
+                ->pause($this->shortDelay)
+                ->keys('@barcode-input-field', '4001')
+                ->keys('@barcode-input-field', '{ENTER}')
+                ->pause($this->shortDelay)
+                ->keys('@barcode-input-field', '4001')
+                ->keys('@barcode-input-field', '{ENTER}')
+                ->pause($this->shortDelay)
+                ->keys('@barcode-input-field', '4001')
+                ->keys('@barcode-input-field', '{ENTER}')
+                ->pause($this->shortDelay)
+                ->click('#options-button')
+                ->pause($this->shortDelay)
+                ->assertSourceMissing('Server Error');
         });
     }
 
     /**
      * @throws Throwable
      */
-    public function testBasics(): void
+    public function testUserAccess(): void
     {
         $this->basicUserAccessTest($this->uri, true);
+    }
+
+    /**
+     * @throws Throwable
+     */
+    public function testAdminAccess(): void
+    {
         $this->basicAdminAccessTest($this->uri, true);
+    }
+
+    /**
+     * @throws Throwable
+     */
+    public function testGuestAccess(): void
+    {
         $this->basicGuestAccessTest($this->uri);
     }
 }
