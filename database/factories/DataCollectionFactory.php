8a42df5fe83749c71774962333579954e0a88de7
Rizky Puji Raharja <rizky.rsj@gmail.com>
2024-10-13 01:01:38 +0700

    Add OrderAddress to DataCollectionFactory (#627)

--- database/factories/DataCollectionFactory.php
+++ database/factories/DataCollectionFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\OrderAddress;
 use App\Models\Warehouse;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
@@ -10,11 +11,14 @@ class DataCollectionFactory extends Factory
     public function definition(): array
     {
         $warehouse = Warehouse::first() ?? Warehouse::factory()->create();
+        $orderAddresses = OrderAddress::factory(2)->create();
 
         return [
             'warehouse_code' => $warehouse->code,
             'warehouse_id' => $warehouse->getKey(),
             'name' => $this->faker->word(),
+            'shipping_address_id' => $orderAddresses[0]->getKey(),
+            'billing_address_id' => $orderAddresses[1]->getKey(),
         ];
     }
 }
